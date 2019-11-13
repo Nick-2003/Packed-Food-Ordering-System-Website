c0c1673dad195f19e98002d4dda3b429d459b3df
Nick-2003 <55626561+Nick-2003@users.noreply.github.com>
2019-11-13 17:45:15 +0000

    pupil

--- addpupil.php
+++ addpupil.php
@@ -5,19 +5,18 @@
 </head>
 <body> 
 <?php
+//header('Location: pupil.php');
 echo $_POST["forename"]."<br>";
 echo $_POST["surname"]."<br>";
-echo $_POST["ID"]."<br>";
 echo $_POST["house"]."<br>";
 try{
 	include_once('connection.php');
 	array_map("htmlspecialchars", $_POST);
 	}
 $stmt = $conn->prepare("INSERT INTO Pupil
-(ID,Surname,Forename,House) VALUES (null,:surname,:forename,:house)");
+(ID,Surname,Forename,House)VALUES (NULL,:surname,:forename,:house)");
 $stmt->bindParam(':forename', $_POST[“forename”]);
 $stmt->bindParam(':surname', $_POST[“surname”]);
-$stmt->bindParam(':ID', $_POST[“ID”]);
 $stmt->bindParam(':house', $_POST[“house”]);
 $stmt->execute();
 	}
