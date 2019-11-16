3bfba9da43cc85fe7eea5604e0e10596f42a9a1c
Nick-2003 <55626561+Nick-2003@users.noreply.github.com>
2019-11-16 08:49:15 +0000

    php

--- additems.php
+++ additems.php
@@ -9,12 +9,40 @@ header("Refresh:2; url= items.php");
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+//sandwich
+$number=1;
+include_once('connection.php');
+array_map("htmlspecialchars", $_POST);
 try{
-	include_once('connection.php');
-	array_map("htmlspecialchars", $_POST);
+	
+	
 	$stmt = $conn->prepare("INSERT INTO lunchitems (OrderID,FoodID)VALUES (:orderid,:foodid)");
-	$stmt->bindParam(':orderid', $_POST["order"]);
-	$stmt->bindParam(':foodid', $_POST["food"]);
+	$stmt->bindParam(':orderid', $number);
+	$stmt->bindParam(':foodid', $_POST["sandwich"]);
+	$stmt->execute();
+	}
+catch(PDOException $e)
+	{
+		echo "error".$e->getMessage();
+	}
+try{
+	
+	
+	$stmt = $conn->prepare("INSERT INTO lunchitems (OrderID,FoodID)VALUES (:orderid,:foodid)");
+	$stmt->bindParam(':orderid', $number);
+	$stmt->bindParam(':foodid', $_POST["drink"]);
+	$stmt->execute();
+	}
+catch(PDOException $e)
+	{
+		echo "error".$e->getMessage();
+	}
+try{
+	
+	
+	$stmt = $conn->prepare("INSERT INTO lunchitems (OrderID,FoodID)VALUES (:orderid,:foodid)");
+	$stmt->bindParam(':orderid', $number);
+	$stmt->bindParam(':foodid', $_POST["snack"]);
 	$stmt->execute();
 	}
 catch(PDOException $e)
