b16aff99aaa4cb2f3550d67be96a085eecf88580
Nick-2003 <55626561+Nick-2003@users.noreply.github.com>
2019-11-27 10:56:35 +0000

    order

--- addorders.php
+++ addorders.php
@@ -16,7 +16,7 @@ error_reporting(E_ALL);
 try{
 	include_once('connection.php');
 	array_map("htmlspecialchars", $_POST);
-	$stmt = $conn->prepare("INSERT INTO LunchOrder(Pupil_ID,Order_ID,CollectionDate,CollectionTime,FoodPrepared,FoodCollected)VALUES (:pupilid,:orderid,collectiondate,collectiontime,foodprepared,foodcollected)");
+	$stmt = $conn->prepare("INSERT INTO LunchOrder(Pupil_ID,Order_ID,CollectionDate,CollectionTime,FoodPrepared,FoodCollected)VALUES (:pupilid,:orderid,:collectiondate,:collectiontime,:foodprepared,:foodcollected)");
 	$stmt->bindParam(':pupilid', $_POST["pupil"]);
 	$stmt->bindParam(':orderid', $_POST["order"]);
 	$stmt->bindParam(':collectiondate', $_POST["collectiondate"]);
